64b8ee7ef196e5e5d31bcb431218bd7be275ec0c
Sandeep-174 <sandeepnaik174@gmail.com>
2022-03-13 18:29:16 +0530

    My First Commit

---
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void postJIRA()
+	{
+		System.out.println("postJIRA");
+	}
+	
+	@Test
+	public void removeTwitter()
+	{
+		System.out.println("removeTwitter");
+	}
+	
+}
